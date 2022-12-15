cbca4bff384aacab5b5fcae38945be88b8981214
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-12-15 03:13:32 +0100

    Replace process.exit with exception

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -22,9 +22,7 @@ async function main() {
   await contract.deployed()
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error)
-    process.exit(1)
-  })
+main().catch((error) => {
+  console.error(error)
+  throw error
+})
