d2e83183bc66997830b21025408b3ece5fcdb48a
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-12-29 08:59:04 +0100

    Add ABIs and types

---
+++ functions/src/abi/index.ts
@@ -0,0 +1,9 @@
+import InferusNamesABI from './InferusNames.json'
+import SubscriptionManagerABI from './SubscriptionManager.json'
+import TestTokenWithPermitABI from './TestTokenWithPermit.json'
+
+export { InferusNames } from './InferusNames'
+export { SubscriptionManager } from './SubscriptionManager'
+export { TestTokenWithPermit } from './TestTokenWithPermit'
+
+export { InferusNamesABI, SubscriptionManagerABI, TestTokenWithPermitABI }
