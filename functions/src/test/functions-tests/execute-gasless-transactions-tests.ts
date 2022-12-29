5b94601fa4aac312f46cc8a557e9c53db3a94831
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-12-29 09:07:59 +0100

    Add tests for `executeGaslessTransaction` and validators

---
+++ functions/src/test/functions-tests/execute-gasless-transactions-tests.ts
@@ -0,0 +1,97 @@
+/* eslint-disable no-unused-expressions,node/no-unpublished-import */
+import { before, describe } from 'mocha'
+import { expect } from 'chai'
+import * as dotenv from 'dotenv'
+import pino from 'pino'
+import { arrayify, hexlify, parseEther, randomBytes } from 'ethers/lib/utils'
+import { Contract, Wallet } from 'ethers'
+import { generatePermitSignature, getExecutor, getProvider } from '../../utils/transactions'
+import { executeGaslessTransaction } from '../../functions/execute-gasless-transactions'
+import {
+  InferusNames,
+  InferusNamesABI,
+  SubscriptionManager,
+  SubscriptionManagerABI,
+  TestTokenWithPermit,
+  TestTokenWithPermitABI,
+} from '../../abi'
+
+const logger = pino()
+dotenv.config()
+
+describe('execute-gasless-transactions tests', () => {
+  let inferusNames: InferusNames
+  let subscriptionManager: SubscriptionManager
+  let testTokenWithPermit: TestTokenWithPermit
+  const alice = Wallet.createRandom()
+
+  before(() => {
+    inferusNames = new Contract(
+      process.env.NAMES_CONTRACT_ADDRESS!,
+      InferusNamesABI,
+      getProvider()
+    ) as InferusNames
+
+    subscriptionManager = new Contract(
+      process.env.SUBSCRIPTIONS_CONTRACT_ADDRESS!,
+      SubscriptionManagerABI,
+      getExecutor()
+    ) as SubscriptionManager
+
+    testTokenWithPermit = new Contract(
+      process.env.TEST_TOKEN_CONTRACT_ADDRESS!,
+      TestTokenWithPermitABI,
+      getProvider()
+    ) as TestTokenWithPermit
+  })
+
+  describe('executeGaslessTransaction', () => {
+    it('should register name successfully', async () => {
+      const name = randomBytes(32)
+      const metadataUri = randomBytes(50)
+      const hash = await inferusNames.getHashForRegisterBySignature(
+        name,
+        alice.address,
+        metadataUri
+      )
+      const signature = await alice.signMessage(arrayify(hash))
+      const result = await executeGaslessTransaction(
+        {
+          transactionType: 'register',
+          arguments: [hexlify(name), alice.address, hexlify(metadataUri), signature],
+        },
+        {
+          logger,
+          requestId: hexlify(randomBytes(40)),
+        }
+      )
+      expect(result.status).to.be.true
+    })
+
+    it('should subscribe successfully', async () => {
+      const amount = parseEther('0.8')
+      const duration = 60
+
+      await subscriptionManager.createPlan(duration, testTokenWithPermit.address, amount, true)
+      const planId = await subscriptionManager.lastId()
+      await testTokenWithPermit.connect(getExecutor()).mint(alice.address, amount)
+      const [v, r, s] = await generatePermitSignature(
+        subscriptionManager,
+        testTokenWithPermit,
+        alice,
+        planId
+      )
+      const result = await executeGaslessTransaction(
+        {
+          transactionType: 'subscribe',
+          arguments: [planId.toHexString(), alice.address, v, r, s],
+        },
+        {
+          logger,
+          requestId: hexlify(randomBytes(40)),
+        }
+      )
+      expect(result.status).to.be.true
+    })
+  })
+})
---
+++ functions/src/test/utils-tests.ts
@@ -0,0 +1,55 @@
+/* eslint-disable no-unused-expressions */
+import { describe } from 'mocha'
+import { expect } from 'chai'
+import { hexStringValidator, isNumber } from '../utils'
+
+describe('utils tests', () => {
+  describe('isNumber', () => {
+    it('should return false when the data is not a javascript number and is not a valid hex number', () => {
+      const data = '0x123'
+      const result = isNumber(data)
+      expect(result).to.be.false
+    })
+
+    it('should return true when the data is a javascript number', () => {
+      const data = 4
+      const result = isNumber(data)
+      expect(result).to.be.true
+    })
+
+    it('should return true when the data is a valid hex number', () => {
+      const data = '0x1234'
+      const result = isNumber(data)
+      expect(result).to.be.true
+    })
+
+    it('should return true when the data is the trivial hex number', () => {
+      const data = '0x'
+      const result = isNumber(data)
+      expect(result).to.be.true
+    })
+  })
+
+  describe('hexStringValidator', () => {
+    it('should return true when the data is a valid hex with the correct length of bytes', () => {
+      const data = '0x6162636400000000000000000000000000000000000000000000000000000000'
+      const validator = hexStringValidator(32)
+      const result = validator(data)
+      expect(result).to.be.true
+    })
+
+    it('should return false when the data is not a valid hex', () => {
+      const data = '3k43'
+      const validator = hexStringValidator(2)
+      const result = validator(data)
+      expect(result).to.be.false
+    })
+
+    it('should return false when the data a valid hex but has the wrong length', () => {
+      const data = '0x123456'
+      const validator = hexStringValidator(2)
+      const result = validator(data)
+      expect(result).to.be.false
+    })
+  })
+})
