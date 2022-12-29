501a7bc8be5230601085475c47ed41440b66a834
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2022-12-29 08:54:26 +0100

    Deploy names contract and subscription manager contract in same script

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -2,7 +2,7 @@ import '@nomiclabs/hardhat-ethers'
 import '@openzeppelin/hardhat-upgrades'
 import { ethers, upgrades } from 'hardhat'
 
-async function main() {
+async function deployInferusNamesContract() {
   const factory = await ethers.getContractFactory('InferusNames')
 
   const basePrice = ethers.utils.parseEther('0.5') // 0.5 MATIC <~0.2-0.8 USD>
@@ -13,13 +13,36 @@ async function main() {
   })
 
   // The address the Contract WILL have once mined
-  console.log(contract.address)
+  console.log('Names Contract:', contract.address)
 
   // The transaction that was sent to the network to deploy the Contract
-  console.log(contract.deployTransaction.hash)
+  console.log('Names Contract tx hash:', contract.deployTransaction.hash)
 
   // The contract is NOT deployed yet; we must wait until it is mined
   await contract.deployed()
+}
+
+async function deploySubscriptionManagerContract() {
+  const factory = await ethers.getContractFactory('SubscriptionManager')
+
+  // If we had constructor arguments, they would be passed into deploy()
+  const contract = await upgrades.deployProxy(factory, [], {
+    initializer: 'initialize',
+  })
+
+  // The address the Contract WILL have once mined
+  console.log('Subscription Manager:', contract.address)
+
+  // The transaction that was sent to the network to deploy the Contract
+  console.log('Subscription Manager tx hash:', contract.deployTransaction.hash)
+
+  // The contract is NOT deployed yet; we must wait until it is mined
+  await contract.deployed()
+}
+
+async function main() {
+  await deployInferusNamesContract()
+  await deploySubscriptionManagerContract()
 }
 
 main().catch((error) => {
