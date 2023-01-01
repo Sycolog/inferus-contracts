698f3ccd2f2dbca03bc13b13ab7c07efbf6ef8d7
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2023-01-01 23:29:48 +0100

    Add dynamic env vars resolution

--- functions/src/execute-gasless-transactions.ts
+++ functions/src/execute-gasless-transactions.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access */
 import { AWSLambdaHTTPEvent } from './types'
 import { createLogger, RequestContext, verifyCaptchaToken } from './utils'
 import { executeGaslessTransaction } from './functions/execute-gasless-transactions'
@@ -17,7 +18,7 @@ async function validateRecaptchaToken(event: AWSLambdaHTTPEvent, context: Reques
     }
   }
 
-  context.logger.trace('validateRecaptchaToken:tokenFound')
+  context.logger.trace('validateRecaptchaToken:token found')
   const isValid = await verifyCaptchaToken(token, context.logger)
   if (!isValid) {
     context.logger.error('validateRecaptchaToken:INVALID_RECAPTCHA_TOKEN')
@@ -31,21 +32,38 @@ async function validateRecaptchaToken(event: AWSLambdaHTTPEvent, context: Reques
   }
 }
 
-export async function handler(event: AWSLambdaHTTPEvent) {
+function loadEnvironmentVariables(context: RequestContext) {
+  try {
+    const envVars = JSON.parse(process.env.ENV_VARS!)
+    for (const key of Object.keys(envVars)) {
+      process.env[key] = envVars[key]
+    }
+    context.logger.info('handler:loaded env vars')
+  } catch (e) {
+    context.logger.error({
+      msg: 'FAILED_TO_LOAD_AWS_ENV_VARS',
+      error: e,
+    })
+  }
+}
+
+export async function handler(
+  event: AWSLambdaHTTPEvent
+): Promise<{ statusCode: number; body: string }> {
   const context = {
     logger: createLogger({
       context: event.requestContext,
     }),
     requestId: event.requestContext.requestId,
   }
-
   context.logger.trace('handler:started')
+  loadEnvironmentVariables(context)
   const tokenValidationError = await validateRecaptchaToken(event, context)
   if (tokenValidationError) {
     return tokenValidationError
   }
 
-  context.logger.info('handler:tokenValidationSuccessful')
+  context.logger.info('handler:token validation successful')
   if (!event.body) {
     context.logger.error('handler:INVALID_BODY:EMPTY_BODY')
     return {
@@ -61,8 +79,11 @@ export async function handler(event: AWSLambdaHTTPEvent) {
   try {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     body = JSON.parse(event.body)
-  } catch {
-    context.logger.error('handler:INVALID_BODY:INVALID_JSON_DATA')
+  } catch (e) {
+    context.logger.error({
+      msg: 'handler:INVALID_BODY:INVALID_JSON_DATA',
+      error: e,
+    })
     return {
       statusCode: 400,
       body: JSON.stringify({
@@ -72,14 +93,29 @@ export async function handler(event: AWSLambdaHTTPEvent) {
     }
   }
 
-  const result = await executeGaslessTransaction(body, context)
-  return {
-    statusCode: result.status ? 200 : 400,
-    body: result.status
+  try {
+    const result = await executeGaslessTransaction(body, context)
+    const response = result.status
       ? result
       : {
-          error: 'EXECUTIION_ERROR',
+          error: 'EXECUTION_ERROR',
           message: result.message,
-        },
+        }
+    return {
+      statusCode: result.status ? 200 : 400,
+      body: JSON.stringify(response),
+    }
+  } catch (e) {
+    context.logger.error({
+      msg: 'handler:executeGaslessTransaction:UNKNOWN_ERROR_OCCURRED',
+      error: e,
+    })
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: 'INTERNAL_SERVER_ERROR',
+        message: 'An internal error occurred. We are investigating the cause of the problem.',
+      }),
+    }
   }
 }
