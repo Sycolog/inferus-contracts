a21717a37d53839755cf51c94d52db0520959a6c
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2023-02-26 23:52:45 +0000

    Add function to create provider from chainId

--- inferus-client/src/engine/index.ts
+++ inferus-client/src/engine/index.ts
@@ -1,6 +1,9 @@
 import { chainsList } from '../index'
+import * as utils from './utils'
+import { providers } from 'ethers'
 let chainsMap: Map<string, typeof chainsList[number]>
 
+export { utils }
 export function getChainsMap(): Map<string, typeof chainsList[number]> {
   if (chainsMap) {
     return chainsMap
@@ -11,4 +14,20 @@ export function getChainsMap(): Map<string, typeof chainsList[number]> {
     chainsMap.set(chain.id, chain)
   }
   return chainsMap
+}
+
+export function getProviderByChainId(config: { chainId?: number }): providers.Provider | undefined {
+  if (!config.chainId) {
+    config.chainId = 1
+  }
+  const chain = getChainsMap().get(`evm:${config.chainId}`)
+  const rpcUrl = chain?.evmMeta?.rpc?.[0]
+  if (!rpcUrl) {
+    return undefined
+  }
+
+  return new providers.JsonRpcProvider(rpcUrl, {
+    chainId: config.chainId,
+    name: chain.name,
+  })
 }
